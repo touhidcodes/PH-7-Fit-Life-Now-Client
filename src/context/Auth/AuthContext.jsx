6fa98c1bdc5ed8c225dc82ff2d7e8623551e4552
touhidcodes <touhidcodes@gmail.com>
2023-06-27 23:26:25 +0600

    auth context added

---
+++ src/context/Auth/AuthContext.jsx
@@ -0,0 +1,76 @@
+import React, { useContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import {
+	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
+	getAuth,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	signOut,
+	updateProfile,
+} from 'firebase/auth';
+
+export const AuthProvider = useContext(null);
+const googleProvider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const AuthContext = ({ children }) => {
+	const [user, setUser] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	const createUser = (email, password) => {
+		setLoading(true);
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+	const signInUser = (email, password) => {
+		setLoading(true);
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+	const logOut = () => {
+		return signOut(auth);
+	};
+	const googleSignIn = () => {
+		return signInWithPopup(auth, googleProvider);
+	};
+	const updateUserProfile = (name, photo) => {
+		return updateProfile(auth.currentUser, {
+			displayName: name,
+			photoURL: photo,
+		});
+	};
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			// console.log("auth state changed", currentUser)
+			setUser(currentUser);
+			setLoading(false);
+			// Set JWT to Backend
+			// if (currentUser) {
+			// 	axiosBase.post('/jwt', { email: currentUser.email }).then((data) => {
+			// 		localStorage.setItem('access_token', data.data.token);
+			// 	});
+			// } else {
+			// 	localStorage.removeItem('access_token');
+			// }
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
+	const authInfo = {
+		auth,
+		user,
+		loading,
+		createUser,
+		signInUser,
+		logOut,
+		googleSignIn,
+		updateUserProfile,
+	};
+	return (
+		<AuthProvider.Provider value={authInfo}>{children}</AuthProvider.Provider>
+	);
+};
+
+export default AuthContext;
